4b7ad4fc9e4e2a2c064fd5170893ae9c5dfc8452
Anindya Dey <andy.ani1992@gmail.com>
2022-07-02 11:19:26 +0530

    add solution for array left rotation

--- arrays/left-rotate/left-rotate.cpp
+++ arrays/left-rotate/left-rotate.cpp
@@ -11,70 +11,49 @@ int main() {
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
 
-    // Get the number of elements in array
-    int numberOfElementsInArray;
-    cin >> numberOfElementsInArray;
+    int inputArraySize, rotateBy;
 
-    // Initialize the array
-    vector<int> inputArray(numberOfElementsInArray);
-    
-    // Store the rotation amount
-    int rotateBy;
+    cin >> inputArraySize;
     cin >> rotateBy;
 
-    // Fill up the array
-    for(int i = 0; i < numberOfElementsInArray; i++) {
+    vector<int> inputArray(inputArraySize);
+
+    for(int i = 0; i < inputArraySize; i++) {
         cin >> inputArray[i];
     }
 
-    // Solve 1
+    // Solution 1
     cout << "Solution 1: " << endl;
     solve1(inputArray, rotateBy);
 
     cout << endl << endl;
 
-    // Solve 2
+    // Solution 2
     cout << "Solution 2: " << endl;
     solve2(inputArray, rotateBy);
 
     return 0;
 }
 
-/**
- * ------------------------------
- * n: Size of input array
- * 
- * Time Complexity: O(n)
- * Space Complexity: O(n)
- * ------------------------------
- */
 void solve1(vector<int> arr, int d) {
     int n = arr.size();
     vector<int> result(n);
 
     for(int i = 0; i < n; i++) {
-        result[i] = arr[(i + (n - d)) % n];
+        result[i] = arr[(i + d) % n];
     }
 
-    for(int e: result) cout << e << " ";
+    for(int el : result) cout << el << " ";
 }
 
-/**
- * ------------------------------
- * n: Size of input array
- * 
- * Time Complexity: O(n)
- * Space Complexity: O(1)
- * ------------------------------
- */
 void solve2(vector<int> arr, int d) {
     int n = arr.size();
 
-    reverse(arr, 0, n - d - 1);
-    reverse(arr, n - d, n - 1);
+    reverse(arr, 0, d - 1);
+    reverse(arr, d, n - 1);
     reverse(arr, 0, n - 1);
 
-    for(int e: arr) cout << e << " ";
+    for(int el : arr) cout << el << " ";
 }
 
 void reverse(vector<int> &arr, int lo, int hi) {
